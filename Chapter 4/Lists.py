ceb6e55844ef2751a6cbc79a187e57dbc50218a0
ASHWATTHAMMA <161590760+ASHWATTHAMMA@users.noreply.github.com>
2025-05-11 14:27:25 +0530

    Initial commit with practice file

---
+++ Chapter 4/Lists.py
@@ -0,0 +1,7 @@
+names = ["GOurav",213,23.2,True,"Sigma boi"]
+print(names)
+# names.reverse()
+# names.append("<NAME>")
+names.insert(2,"Gigga gigga gigga>")
+names.pop(3)
+print(names)
---
+++ Chapter 4/Tuples.py
@@ -0,0 +1,10 @@
+a = (1,2,3,4,5)
+b = (1,)
+print(type(a))
+print(a)
+print(type(b))
+print(b)
+
+# tuples methods
+no  =  a.count(4)
+print(no)
